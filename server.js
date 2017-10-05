dd06b669e2ec4361aa1782d782ab783bf9cb3cc5
Luciano Olmedo <luciano.federico.olmedo@gmail.com>
2017-10-05 00:19:13 -0300

    Changing server.js properties

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var app = express();
 
-app.use(express.static(__dirname + "/dist" ));
+app.use(express.static(__dirname ));
 
 //app.use(express.static(__dirname + "/modules"));
 app.use('/bower_components', express.static(__dirname + '/bower_components'));
